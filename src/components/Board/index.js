561182a7b35f5babf79b19f7f83e26a45f610388
haruelrovix <surat@havit.web.id>
2021-10-14 15:21:59 +0700

    fix: Change to Class component due to race condition issue using Hooks

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -15,22 +15,34 @@ import {
   GAME_RESULT_TIE,
   GAME_RESULT_USER,
 } from '../../constants/game';
+import Billboard from '../Billboard';
 import Circle from '../Circle';
 import Cross from '../Cross';
 
-const Board = () => {
-  const [userInputs, setUserInputs] = useState([]);
-  const [AIInputs, setAIInputs] = useState([]);
-  const [result, setResult] = useState(GAME_RESULT_NO);
+const isWinner = inputs =>
+  CONDITIONS.some(condition =>
+    condition.every(item => inputs.indexOf(item) !== -1),
+  );
 
-  const handleBoardClick = e => {
-    const { locationX, locationY } = e.nativeEvent;
+export default class Board extends Component {
+  constructor() {
+    super();
+    this.state = {
+      AIInputs: [],
+      userInputs: [],
+      result: GAME_RESULT_NO,
+    };
+  }
+
+  handleBoardClick = e => {
+    const { userInputs, AIInputs, result } = this.state;
 
     if (result !== -1) {
       return;
     }
 
     const inputs = [...userInputs, ...AIInputs];
+    const { locationX, locationY } = e.nativeEvent;
 
     const area = AREAS.find(
       location =>
@@ -41,33 +53,28 @@ const Board = () => {
     );
 
     if (area && inputs.every(location => location !== area.id)) {
-      setUserInputs([...userInputs, area.id]);
+      this.setState({ userInputs: [...userInputs, area.id] });
+
       setTimeout(() => {
-        findTheWinner();
-        runEnemyMove();
-      }, 1000);
+        this.findTheWinner();
+        this.runEnemyMove();
+      }, 5);
     }
   };
 
-  const isWinner = inputs =>
-    CONDITIONS.some(condition =>
-      condition.every(item => inputs.indexOf(item) !== -1),
-    );
-
-  const findTheWinner = () => {
+  findTheWinner = () => {
+    const { userInputs, AIInputs, result } = this.state;
     const inputs = [...userInputs, ...AIInputs];
 
     if (inputs.length >= 5) {
       let res = isWinner(userInputs);
       if (res && result !== GAME_RESULT_USER) {
-        setResult(GAME_RESULT_USER);
-        return;
+        return this.setState({ result: GAME_RESULT_USER });
       }
 
       res = isWinner(AIInputs);
       if (res && result !== GAME_RESULT_AI) {
-        setResult(GAME_RESULT_AI);
-        return;
+        return this.setState({ result: GAME_RESULT_AI });
       }
     }
 
@@ -76,11 +83,12 @@ const Board = () => {
       result === GAME_RESULT_NO &&
       result !== GAME_RESULT_TIE
     ) {
-      setResult(GAME_RESULT_TIE);
+      this.setState({ result: GAME_RESULT_TIE });
     }
   };
 
-  const runEnemyMove = () => {
+  runEnemyMove = () => {
+    const { userInputs, AIInputs, result } = this.state;
     if (result !== -1) {
       return;
     }
@@ -91,16 +99,18 @@ const Board = () => {
       const randomNumber = Math.round(Math.random() * 8.3);
 
       if (inputs.every(input => input !== randomNumber)) {
-        setAIInputs([...AIInputs, randomNumber]);
-        findTheWinner();
+        // setAIInputs([...AIInputs, randomNumber]);
+        this.setState({ AIInputs: [...AIInputs, randomNumber] });
+
+        this.findTheWinner();
         break;
       }
     }
   };
 
-  const renderCircle = (inputs, Component) => {
+  markInput = (inputs, Comp) => {
     return inputs.map((input, index) => (
-      <Component
+      <Comp
         key={index}
         translateX={CENTER_POINTS[input].x}
         translateY={CENTER_POINTS[input].y}
@@ -109,50 +119,55 @@ const Board = () => {
     ));
   };
 
-  return (
-    <SafeAreaView>
-      <View style={styles.container}>
-        <TouchableWithoutFeedback onPress={e => handleBoardClick(e)}>
-          <View style={styles.boardContainer}>
-            <View style={styles.line} />
-            <View
-              style={[
-                styles.line,
-                {
-                  width: 3,
-                  height: 306,
-                  transform: [{ translateX: 200 }],
-                },
-              ]}
-            />
-            <View
-              style={[
-                styles.line,
-                {
-                  width: 306,
-                  height: 3,
-                  transform: [{ translateY: 100 }],
-                },
-              ]}
-            />
-            <View
-              style={[
-                styles.line,
-                {
-                  width: 306,
-                  height: 3,
-                  transform: [{ translateY: 200 }],
-                },
-              ]}
-            />
-            {renderCircle(userInputs, Circle)}
-            {renderCircle(AIInputs, Cross)}
-          </View>
-        </TouchableWithoutFeedback>
-      </View>
-    </SafeAreaView>
-  );
-};
+  render() {
+    const { userInputs, AIInputs, result } = this.state;
+
+    return (
+      <SafeAreaView>
+        <View style={styles.container}>
+          <TouchableWithoutFeedback onPress={this.handleBoardClick}>
+            <View style={styles.boardContainer}>
+              <View style={styles.line} />
+              <View
+                style={[
+                  styles.line,
+                  {
+                    width: 3,
+                    height: 306,
+                    transform: [{ translateX: 200 }],
+                  },
+                ]}
+              />
+              <View
+                style={[
+                  styles.line,
+                  {
+                    width: 306,
+                    height: 3,
+                    transform: [{ translateY: 100 }],
+                  },
+                ]}
+              />
+              <View
+                style={[
+                  styles.line,
+                  {
+                    width: 306,
+                    height: 3,
+                    transform: [{ translateY: 200 }],
+                  },
+                ]}
+              />
+              {this.markInput(userInputs, Circle)}
+              {this.markInput(AIInputs, Cross)}
+            </View>
+          </TouchableWithoutFeedback>
+          <Billboard result={result} />
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -172,5 +187,3 @@ const styles = StyleSheet.create({
     transform: [{ translateX: 100 }],
   },
 });
-
-export default Board;
