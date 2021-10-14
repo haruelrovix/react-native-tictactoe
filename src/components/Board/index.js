15d504a07bea27b00a092464bc10b938d919aa0b
haruelrovix <surat@havit.web.id>
2021-10-14 13:40:35 +0700

    feat: Add clickable area

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,43 +1,57 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet, TouchableWithoutFeedback, View } from 'react-native';
 
 const Board = () => {
+  const [userInputs, setUserInputs] = useState([])
+
+  const handleBoardClick = e => {
+    const { locationX, locationY } = e.nativeEvent
+    console.log({ locationX })
+    console.log({ locationY })
+
+    setUserInputs([...userInputs, { locationX, locationY }])
+  }
+
+  console.log({ userInputs })
+
   return (
     <SafeAreaView>
       <View style={styles.container}>
-        <View style={styles.boardContainer}>
-          <View style={styles.line} />
-          <View
-            style={[
-              styles.line,
-              {
-                width: 3,
-                height: 306,
-                transform: [{ translateX: 200 }],
-              },
-            ]}
-          />
-          <View
-            style={[
-              styles.line,
-              {
-                width: 306,
-                height: 3,
-                transform: [{ translateY: 100 }],
-              },
-            ]}
-          />
-          <View
-            style={[
-              styles.line,
-              {
-                width: 306,
-                height: 3,
-                transform: [{ translateY: 200 }],
-              },
-            ]}
-          />
-        </View>
+        <TouchableWithoutFeedback onPress={e => handleBoardClick(e)}>
+          <View style={styles.boardContainer}>
+            <View style={styles.line} />
+            <View
+              style={[
+                styles.line,
+                {
+                  width: 3,
+                  height: 306,
+                  transform: [{ translateX: 200 }],
+                },
+              ]}
+            />
+            <View
+              style={[
+                styles.line,
+                {
+                  width: 306,
+                  height: 3,
+                  transform: [{ translateY: 100 }],
+                },
+              ]}
+            />
+            <View
+              style={[
+                styles.line,
+                {
+                  width: 306,
+                  height: 3,
+                  transform: [{ translateY: 200 }],
+                },
+              ]}
+            />
+          </View>
+        </TouchableWithoutFeedback>
       </View>
     </SafeAreaView>
   );
