550027b76d2510122ba6a45b4bec07a1e746c8c7
haruelrovix <surat@havit.web.id>
2021-10-18 17:02:27 +0700

    fix: Issues on Android

--- src/navigation/Main.js
+++ src/navigation/Main.js
@@ -4,6 +4,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Board from '../components/Board';
 import { Icon } from 'react-native-elements';
 import ScoreBoard from '../components/ScoreBoard';
+import { Platform } from 'react-native';
 
 const BoardIcon = () => {
   return <Icon name="play" type="evilicon" />;
@@ -17,7 +18,11 @@ const Tab = createBottomTabNavigator();
 
 const MyTabs = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarItemStyle: { paddingVertical: Platform.select({ android: 4 }) },
+      }}
+    >
       <Tab.Screen
         name="Board"
         component={Board}
